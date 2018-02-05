837f6643ba4a16289f4ec458de8978284735cec4
webadministrator <pkoxex@gmail.com>
2018-02-05 16:59:48 +0200

    test commit

--- node_test/helloworld.js
+++ node_test/helloworld.js
@@ -1,6 +1,8 @@
 var http = require('http');
-http.createServer(function(req,res){
-res.writeHead(200, { 'Content-Type': 'text/html' });
-res.end('<h1>Hello world!</h1>');
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end('<h1>Hello world!</h1>');
 }).listen(3000);
 console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
